a82da746e2b155303e83921545f449255771f8fa
Shuvo-06 <168964479+Shuvo-06@users.noreply.github.com>
2025-09-24 00:20:09 +0600

    Create tools.hpp

---
+++ tools.hpp
@@ -0,0 +1,19 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+#include <string>
+#include <vector>
+
+void clear_screen();
+void read_password(std::string &);
+
+class Encryption {
+    static constexpr uint8_t KEY = 36;
+public:
+    static void encrypt(std::string &password);
+};
+
+void Menu(int choice, int scWidth, int scHeight, const std::vector<std::string> &menu, const std::string &caption);
+int menu(const std::vector<std::string> &menu_options, const std::string &caption = "");
+
+#endif // TOOLS_H
