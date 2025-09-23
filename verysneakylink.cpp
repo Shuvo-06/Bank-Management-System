86c4947d1daccfc0f5caed4383a551175e57b3f2
Juairia2020 <83693493+Juairia2020@users.noreply.github.com>
2025-09-23 23:36:33 +0600

    Delete verysneakylink.cpp

--- verysneakylink.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream.h>
-using namespace std;
-
-int main()
-{
-cout << "https://cdn.discordapp.com/attachments/1199720556514267168/1420097811462094878/images.jpg?ex=68d428a6&is=68d2d726&hm=7aa6bd3e0703c0bbd055884dc2dcea66d0a05cb97359701890a8ca57ac800ec6"<<endl;
-}
