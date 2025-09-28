3215c441fecfda228cd2f3cba10b999684533d32
Juairia2020 <83693493+Juairia2020@users.noreply.github.com>
2025-09-28 17:02:56 +0600

    Update tools.hpp

--- tools.hpp
+++ tools.hpp
@@ -4,6 +4,18 @@
 #include <string>
 #include <vector>
 
+#define MAX_PASSWORD_SIZE 100
+#define RESET   "\033[0m"
+#define BOLD    "\033[1m"
+
+#define RED "\033[31m"
+#define YELLOW "\033[33m"
+#define GREEN "\033[92m"
+#define CYAN    "\033[36m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[95m"
+
+
 void clear_screen();
 void go_back();
 void read_password(std::string &);
@@ -16,5 +28,8 @@ public:
 
 void Menu(int choice, int scWidth, int scHeight, const std::vector<std::string> &menu, const std::string &caption);
 int menu(const std::vector<std::string> &menu_options, const std::string &caption = "");
+void Msg(const std::string &msg, const std::string &msgNature = "");
+void header(const std::string &msg, std::string color = RESET, int width = 60);
+void subHeader(const std::string &msg, std::string color = RESET, int width = 60);
 
 #endif // TOOLS_H
