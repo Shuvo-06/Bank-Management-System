fe83c7ace4abc9e21b81e8da4808414982fd3665
Juairia2020 <83693493+Juairia2020@users.noreply.github.com>
2025-09-30 14:52:28 +0600

    Add files via upload

--- tools.hpp
+++ tools.hpp
@@ -1,27 +1,38 @@
 #ifndef TOOLS_H
 #define TOOLS_H
 
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
+// -- Color codes
+// cout << COLOR_NAME << ... << RESET
 #define MAX_PASSWORD_SIZE 100
 #define RESET   "\033[0m"
 #define BOLD    "\033[1m"
-
-#define RED "\033[31m"
+#define RED     "\033[31m"
 #define YELLOW "\033[33m"
 #define GREEN "\033[92m"
 #define CYAN    "\033[36m"
 #define BLUE    "\033[34m"
 #define MAGENTA "\033[95m"
 
+#ifdef _WIN32
+    #ifndef NOMINMAX
+        #define NOMINMAX
+    #endif
+    #define WIN32_LEAN_AND_MEAN // cut down Windows headers
+    #define byte win_byte_override // stop windows.h from defining 'byte'
+    #include <windows.h>
+    BOOL WINAPI ConsoleHandler(DWORD signal);
+#endif
+
+#define ENTRY_LIMIT 2
 
 void clear_screen();
 void go_back();
 void read_password(std::string &);
 
 class Encryption {
-    static constexpr uint8_t KEY = 36;
+    static constexpr int KEY = 36;
 public:
     static void encrypt(std::string &password);
 };
@@ -31,5 +42,16 @@ int menu(const std::vector<std::string> &menu_options, const std::string &captio
 void Msg(const std::string &msg, const std::string &msgNature = "");
 void header(const std::string &msg, std::string color = RESET, int width = 60);
 void subHeader(const std::string &msg, std::string color = RESET, int width = 60);
+void read_entire_file(const std::string &);
 
+bool check_name_validity(const std::string &n);
+bool check_pass_validity(std::string p);
+bool check_nid_validity(std::string nid);
+
+void save_accounts();
+void save_employees();
+void save_histories();
+void save_rates();
+void load_complaints();
+void save_complaints();
 #endif // TOOLS_H
