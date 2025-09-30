49c8acb36acef589fc8aa811a5171d9a10a62a38
Juairia2020 <83693493+Juairia2020@users.noreply.github.com>
2025-09-30 14:53:36 +0600

    Add files via upload

---
+++ general.hpp
@@ -0,0 +1,43 @@
+#ifndef GENERAL_HPP
+#define GENERAL_HPP
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------------------------------
+// Account Data Struct
+// ---------------------------------
+struct AccountData {
+    string name;
+    string nid;
+    string password;
+    double deposit;
+    vector<double> history;
+};
+
+// Global vector for all account holders
+extern vector<AccountData> Account_vector;
+
+// ---------------------------------
+// Account Holder Class
+// ---------------------------------
+class AccountHolder {
+    string name;
+    string nid;
+    string pass;
+    string conf_pass;
+    double deposit;
+
+public:
+    // Constructors
+    AccountHolder(double deposit = 0.0);       // For sign-up
+    AccountHolder(int index, string nid);      // For sign-in / dashboard
+
+    // Member functions
+    void collect_info();                        // Sign-up info collection
+    static void accntHolderInfo();              // Display all account holder info
+    static void updateCustomer(int);
+    static void complainBox(string);
+};
+
+#endif // GENERAL_HPP
