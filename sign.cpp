2da71205aa82c895db9ed1e46488e3c090ea1fe7
Juairia2020 <83693493+Juairia2020@users.noreply.github.com>
2025-10-26 17:20:42 +0600

    Update sign.cpp

--- sign.cpp
+++ sign.cpp
@@ -88,11 +88,6 @@ void SignIn::signin_general() {
 
     Msg("Enter Your NID", "prompt");
     cin >> nid;
-    if(nid=="0"){
-        Msg("Your Account Has Been Deleted","Warning");
-        go_back();
-        return;
-    }
 
     Msg("Enter Password", "prompt");
     read_password(password);
