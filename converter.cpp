1b9bb0a87265ed943717821a0ae50cf18c45890d
MDAIR108 <mdairifat102030@gmail.com>
2025-10-29 00:30:58 +0600

    Update converter.cpp

--- converter.cpp
+++ converter.cpp
@@ -8,6 +8,20 @@
 vector<string> Converter::types;
 vector<double> Converter::rates;
 
+Converter::Converter(double amt, double r) : amount(amt), rate(r) {}
+
+Converter Converter::operator*(const Converter &usd) {
+        Converter temp;
+        temp.amount = amount*usd.rate;
+        return temp;
+}
+
+Converter Converter::operator/(const Converter &usd) {
+        Converter temp;
+        temp.amount = amount / usd.rate;
+        return temp;
+}
+
 void Converter::convert_to_bdt(double &amount, string type) {
     for (int i = 0; i < (int)types.size(); i++) {
         if (types[i] == type) {
@@ -25,3 +39,23 @@ void Converter::convert_from_bdt(double &amount, string type) {
         }
     }
 }
+
+
+
+//void Converter::convert_to_bdt(double &amount, string type) {
+//    for (int i = 0; i < (int)types.size(); i++) {
+//        if (types[i] == type) {
+//            amount *= rates[i];
+//            return;
+//        }
+//    }
+//}
+//
+//void Converter::convert_from_bdt(double &amount, string type) {
+//    for (int i = 0; i < (int)types.size(); i++) {
+//        if (types[i] == type) {
+//            amount /= rates[i];
+//            return;
+//        }
+//    }
+//}
